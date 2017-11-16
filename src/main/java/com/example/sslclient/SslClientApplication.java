bee36a6eb9af1f65e964a219badb6782cece8c35
Domenic <domenicbove@gmail.com>
2017-11-16 13:14:04 -0800

    almost done/cleaned up

--- src/main/java/com/example/sslclient/SslClientApplication.java
+++ src/main/java/com/example/sslclient/SslClientApplication.java
@@ -7,6 +7,8 @@ import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.ssl.SSLContextBuilder;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
@@ -14,37 +16,42 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
 @Configuration
-public class SslClientApplication {
-  
-  @Value("${http.client.ssl.trust-store}")
-  private static Resource keyStore;
-  @Value("${http.client.ssl.trust-store-password}")
-  private static String keyStorePassword;
-  
-  static RestTemplate restTemplate() throws Exception {
-      SSLContext sslContext = new SSLContextBuilder()
-              .loadTrustMaterial(
-                      keyStore.getURL(),
-                      keyStorePassword.toCharArray()
-              ).build();
-      SSLConnectionSocketFactory socketFactory =  new SSLConnectionSocketFactory(sslContext);
-      HttpClient httpClient = HttpClients.custom().setSSLSocketFactory(socketFactory).build();
-      HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
-      return new RestTemplate(factory);
-  }
+@SpringBootApplication
+public class SslClientApplication implements CommandLineRunner {
 
-	public static void main(String[] args) throws Exception {
-	  RestTemplate restTemplate = restTemplate();
-	  
-	  String serverUrl = "https://ssl-server:8443/secured";
-	  ResponseEntity<String> response;
+    @Value("${server.ssl.trust-store}")
+    private Resource keyStore;
+    @Value("${server.ssl.trust-store-password}")
+    private String keyStorePassword;
 
-		for (int i=0; i<20; i++) {
-		    response  = restTemplate.getForEntity(serverUrl, String.class);
-		    System.out.println("Response: " + response.getBody());
-				Thread.sleep(5000);
-		}
+    private RestTemplate restTemplate() throws Exception {
+	SSLContext sslContext = new SSLContextBuilder()
+		.loadTrustMaterial(
+			keyStore.getURL(),
+			keyStorePassword.toCharArray()
+		).build();
+	SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext);
+	HttpClient httpClient = HttpClients.custom().setSSLSocketFactory(socketFactory).build();
+	HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
+	return new RestTemplate(factory);
+    }
+
+    public static void main(String... args) {
+	SpringApplication.run(SslClientApplication.class, args);
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+	RestTemplate restTemplate = restTemplate();
+	String serverUrl = "https://ssl-server." + System.getenv("POD_NAMESPACE") + ".svc:8443/secured";
+	ResponseEntity<String> response;
+
+	for (int i=0; i<20; i++) {
+	    response  = restTemplate.getForEntity(serverUrl, String.class);
+	    System.out.println("Response: " + response.getBody());
+	    Thread.sleep(5000);
 	}
+    }
 }
+

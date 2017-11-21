f8a20cac1bf8d131d2c6ef53fe5cc69d5d81a5c3
Domenic <domenicbove@gmail.com>
2017-11-20 23:08:23 -0800

    code cleaned up

--- src/main/java/com/example/sslclient/SslClientApplication.java
+++ src/main/java/com/example/sslclient/SslClientApplication.java
@@ -2,14 +2,10 @@ package com.example.sslclient;
 
 import java.io.IOException;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,38 +15,18 @@ public class SslClientApplication {
 
 
     public static void main(String[] args) throws ClientProtocolException, IOException, InterruptedException {
-	
-	//HttpClient client = new HttpClient();
+
 	CloseableHttpClient client = HttpClients.createDefault();
-//	GetMethod method = new GetMethod();
-	String uri = "https://ssl-server:8443/secured";
-//	method.setURI(new URI(uri, false));
-	
+	String uri = "https://ssl-server." + System.getenv("POD_NAMESPACE") + ".svc:8443/secured";
 	HttpResponse response;
-	
-	//HttpResponse response = httpClient.execute(new HttpGet(URL));
-//	HttpEntity entity = response.getEntity();
-//	String responseString = EntityUtils.toString(entity, "UTF-8");
-//	System.out.println(responseString);
 
-
-//	HttpResponse response = client.execute(new HttpGet(uri));
-//	HttpEntity entity = response.getEntity();
-//	String responseString = EntityUtils.toString(entity, "UTF-8");
-//	System.out.println(responseString);
-	
-	//for (int i=0; i<30; i++) {
 	while(true) {
-	    
 	    response = client.execute(new HttpGet(uri));
-	    HttpEntity entity = response.getEntity();
-		String responseString = EntityUtils.toString(entity, "UTF-8");
-		System.out.println(responseString);
-	 
+	    String responseString = EntityUtils.toString(response.getEntity(), "UTF-8");
+	    System.out.println(responseString);
 	    Thread.sleep(10000);
 	}
     }
-
 
 }
 

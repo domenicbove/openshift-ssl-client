57ac58e4e48c2cf042e557419a403048b968b794
Domenic <domenicbove@gmail.com>
2017-11-20 22:46:56 -0800

    found a diff way online

--- src/main/java/com/example/sslclient/SslClientApplication.java
+++ src/main/java/com/example/sslclient/SslClientApplication.java
@@ -1,66 +1,56 @@
 package com.example.sslclient;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.URI;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
+import java.io.IOException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @SpringBootApplication
-public class SslClientApplication implements CommandLineRunner {
-
-//    @Value("${server.ssl.trust-store}")
-//    private Resource keyStore;
-//    @Value("${server.ssl.trust-store-password}")
-//    private String keyStorePassword;
-//
-//    private RestTemplate restTemplate() throws Exception {
-//	SSLContext sslContext = new SSLContextBuilder()
-//		.loadTrustMaterial(
-//			keyStore.getURL(),
-//			keyStorePassword.toCharArray()
-//			).build();
-//	SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(sslContext);
-//	HttpClient httpClient = HttpClients.custom().setSSLSocketFactory(socketFactory).build();
-//	HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
-//	return new RestTemplate(factory);
-//    }
-
-    public static void main(String... args) {
-	SpringApplication.run(SslClientApplication.class, args);
-    }
-
-    //public static void main(String[] args) throws HttpException, IOException, InterruptedException {
-
-    //}
+public class SslClientApplication {
 
 
-    @Override
-    public void run(String... strings) throws Exception {
-//	RestTemplate restTemplate = restTemplate();
-//	String serverUrl = "https://ssl-server." + System.getenv("POD_NAMESPACE") + ".svc:8443/secured";
-//	ResponseEntity<String> response;
-//
-//	for (int i=0; i<20; i++) {
-//	    response  = restTemplate.getForEntity(serverUrl, String.class);
-//	    System.out.println("Response: " + response.getBody());
-//	    Thread.sleep(5000);
-//	}
-
-	HttpClient client = new HttpClient();
-	GetMethod method = new GetMethod();
+    public static void main(String[] args) throws ClientProtocolException, IOException, InterruptedException {
+	
+	//HttpClient client = new HttpClient();
+	CloseableHttpClient client = HttpClients.createDefault();
+//	GetMethod method = new GetMethod();
 	String uri = "https://ssl-server:8443/secured";
-	method.setURI(new URI(uri, false));
+//	method.setURI(new URI(uri, false));
+	
+	HttpResponse response;
+	
+	//HttpResponse response = httpClient.execute(new HttpGet(URL));
+//	HttpEntity entity = response.getEntity();
+//	String responseString = EntityUtils.toString(entity, "UTF-8");
+//	System.out.println(responseString);
 
+
+//	HttpResponse response = client.execute(new HttpGet(uri));
+//	HttpEntity entity = response.getEntity();
+//	String responseString = EntityUtils.toString(entity, "UTF-8");
+//	System.out.println(responseString);
+	
 	//for (int i=0; i<30; i++) {
 	while(true) {
-	    client.executeMethod(method);
+	    
+	    response = client.execute(new HttpGet(uri));
+	    HttpEntity entity = response.getEntity();
+		String responseString = EntityUtils.toString(entity, "UTF-8");
+		System.out.println(responseString);
+	 
 	    Thread.sleep(10000);
 	}
-
     }
+
+
 }
 
